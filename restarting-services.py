fb0693efc2e2bc6f6ebfaadf68005fe4a76e6477
Vlad Vorontsov <vladislav.vorontsov@tura-group.ru>
2023-03-18 20:17:52 +0300

    add status comments to restarting-services.py

--- restarting-services.py
+++ restarting-services.py
@@ -1,5 +1,8 @@
 import os
 
 os.system("systemctl daemon-reload")
+os.system("echo Restarting daemon: DONE")
 os.system("systemctl restart gunicorn-apicollection")
+os.system("echo Restarting gunicorn-apicollection: DONE")
 os.system("systemctl restart nginx")
+os.system("echo Restarting nginx: DONE")
